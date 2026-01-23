edd210c26d3fb86c2936ac032a443d8b5d0f41c3
FrankLef <flefebvre01@hotmail.com>
2026-01-23 07:05:37 -0500

    test(log1ps)

--- src/fltk/__init__.py
+++
@@ -1,4 +0,0 @@
-# NOTE: This code causes error message when using pytest to test the package.
-# read version from installed package
-# from importlib.metadata import version
-# __version__ = version("fltk")
