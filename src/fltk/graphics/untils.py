cc8fccfbda4456c891343953f92fe4f6fe45e0f6
FrankLef <flefebvre01@hotmail.com>
2026-02-01 18:18:40 -0500

    feat(graohics): utils

---
+++ src/fltk/graphics/untils.py
@@ -0,0 +1,11 @@
+import matplotlib.colors as mcolors
+
+def convert_hex_to_rgba(hex: str, alpha:float|None=None)->str:
+    """Convert hex to a string with rgba, for plotly.
+    
+    Plotly only accept 6-digit hex but pypalettes gives 8-digit hex.
+    """
+    rgba =list(mcolors.to_rgba(hex, alpha=alpha))
+    rgba_str = map(str, rgba)
+    color = "rgba(" + ",".join(rgba_str) + ")"
+    return color
