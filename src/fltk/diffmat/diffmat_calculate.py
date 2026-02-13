d09066f877f969e943d2d239feb6fbb3cff46540
FrankLef <flefebvre01@hotmail.com>
2026-02-13 05:11:32 -0500

    feat(diffmat)

--- src/fltk/diffmat/diffmat_calculate.py
+++ src/fltk/diffmat/diffmat_calculate.py
@@ -10,20 +10,21 @@ def calculate(inst: DiffMat) -> pd.DataFrame:
     newdata = calc_newvalue(inst)
     return newdata
 
-def calc_newvalue(inst:DiffMat)->pd.DataFrame:
+
+def calc_newvalue(inst: DiffMat) -> pd.DataFrame:
     newdata = get_newvalues(inst)
-    
+
     newvalid = pd.merge(
         left=inst._valid_data,
         right=newdata,
         on=inst._data_keys,
-        )
+    )
     # breakpoint()
-    
+
     ndata = inst._valid_data.shape[0]
     nnew = newvalid.shape[0]
     if nnew != ndata:
-        msg:str=f"""
+        msg: str = f"""
         Data and merged df must have the same nb of rows.
         Data has {ndata} rows, merged df has {nnew} rows.
         Weird!
@@ -32,13 +33,14 @@ def calc_newvalue(inst:DiffMat)->pd.DataFrame:
     # breakpoint()
     return newvalid
 
-def get_newvalues(inst:DiffMat)->pd.DataFrame:
+
+def get_newvalues(inst: DiffMat) -> pd.DataFrame:
     df = pd.merge(
-        left=inst._valid_data,
+        left=inst._data,
         right=inst._idx_df,
         left_on=inst._data_idx,
-        right_on=inst._idx_from
-        )
+        right_on=inst._idx_from,
+    )
     df[inst._data_newvalue] = df[inst._idx_coef] * df[inst._data_value]
     newkeys = list(inst._data_group)
     newkeys.append(inst._idx_to)
