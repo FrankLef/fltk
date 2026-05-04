bfc992e1b4bb3ec0099fee4240ee8699fd417934
FrankLef <flefebvre01@hotmail.com>
2026-05-04 18:42:08 -0400

    feat(bridge): init

--- src/fltk/calc_bridge/bridge.py
+++ src/fltk/calc_bridge/bridge.py
@@ -7,6 +7,27 @@ if TYPE_CHECKING:
 
 
 def get_bridge(inst: CalcBridge) -> pd.DataFrame:
-    _data = inst.raw_df
+    _raw = inst.raw_df
     _periods = inst.periods_df
-    return _data
+    _period = inst.raw.period
+    _groups = inst.raw.groups
+    _ratio_nm = inst.raw.ratio
+    _start = inst.periods.start
+    _end = inst.periods.end
+
+    data = _raw[list(inst.raw.vars)]
+    bridge_start = _periods.merge(
+        right=data, how="inner", left_on=_start, right_on=_period
+    )
+    bridge_start.drop(columns=_period, inplace=True)
+
+    bridge_end = _periods.merge(right=data, how="inner", left_on=_end, right_on=_period)
+    bridge_end.drop(columns=[_period, _start], inplace=True)
+
+    _on = (*_groups, _ratio_nm, _end)
+    suffixes = ("_" + inst.bridge.from_sfx, "_" + inst.bridge.to_sfx)
+    bridge_df = bridge_start.merge(
+        right=bridge_end, how="inner", on=_on, suffixes=suffixes
+    )
+    # breakpoint()
+    return bridge_df
