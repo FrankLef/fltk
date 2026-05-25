74cba29dad20d21c7f205a29e7d658f0b5ca1827
FrankLef <flefebvre01@hotmail.com>
2026-05-25 16:04:33 -0400

    feat(utils): scale_format

--- src/fltk/utils/scale_format.py
+++ src/fltk/utils/scale_format.py
@@ -4,12 +4,16 @@
 def scale_format(val: float, scale: float, fmt_str: str) -> str:
     """Multiplies a value by scale and formats it as a string.
 
-    Very useful function used primarily in pandas when formating columns.
+    Very useful function used primarily in pandas when formating columns. See example below.
 
     Args:
         val (float): Value to format.
         scale (float): Scale used to multiply val.
         fmt_str (str): Format string used to format the scaled value.
+
+    Examples:
+
+    >>> df['new'] = df['val'].apply(scale_format, scale=1, fmt_str="{:,.2f}")  # doctest: +SKIP
 
     Returns:
         str: Scaled and formatted value.
