53ce07bd21cde4feafec32d3832b19f41eb8ee2b
FrankLef <flefebvre01@hotmail.com>
2026-05-25 17:44:24 -0400

    test(utils): scale_format

--- tests/test_utils_scale_format.py
+++ tests/test_utils_scale_format.py
@@ -11,5 +11,21 @@ import fltk.utils.scale_format as sc
     ],
 )
 def test_scale_format(val, scale, fmt_str, expected):
+    """test with usual fmt_str."""
+    formatted_val = sc.scale_format(val, scale=scale, fmt_str=fmt_str)
+    assert formatted_val == expected
+
+
+@pytest.mark.parametrize(
+    "val, scale, digits, expected",
+    [
+        [123456, 1e-3, 2, "123.46 M"],
+        [0.123456, 100, 3, "12.346 M"],
+        [123.456789, 1, 1, "123.5 M"],
+    ],
+)
+def test_scale_format_digits(val, scale, digits, expected):
+    """test with digits."""
+    fmt_str = f"{{:.{digits}f}} M"
     formatted_val = sc.scale_format(val, scale=scale, fmt_str=fmt_str)
     assert formatted_val == expected
