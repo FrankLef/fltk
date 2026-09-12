c53e8f16dc1f99455bc93513383c941f07f17a36
FrankLef <flefebvre01@hotmail.com>
2026-09-12 08:29:47 -0400

    ferefactor(flgr)

--- src/fltk/graphics/colrs.py
+++
@@ -1,9 +0,0 @@
-from pypalettes import load_palette
-
-
-def get_colrs(palette_nm: str, colr_no: int | None = None):
-    if colr_no is not None:
-        palette = load_palette("Fun")
-    else:
-        palette = load_palette("Fun")
-    return palette
